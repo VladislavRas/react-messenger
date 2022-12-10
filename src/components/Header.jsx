f3cacd64f6cbf48edeb1fdb0f0713a305856aea8
VladislavRas <99723680+VladislavRas@users.noreply.github.com>
2022-12-10 13:46:48 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://www.clipartmax.com/png/full/299-2997039_subscribe-to-our-email-list-e-mail-icon-png.png" />
+    </header>
+}
+
+export default Header;
+
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src="https://images.unsplash.com/photo-1483653364400-eedcfb9f1f88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" />
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
+
